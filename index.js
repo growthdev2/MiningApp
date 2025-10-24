1dfaa7bfe1f6459f715924ee9ac67a6f7c02ca1d
vaibhav-xi <vaibhav28890@gmail.com>
2025-10-24 18:56:34 +0530

    Clear Everything

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-
-import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('BitPlayPro', () => App);
--- ios/linking.swift
+++
@@ -1,8 +0,0 @@
-//
-//  linking.swift
-//  BitcoinMining
-//
-//  Created by user263593 on 9/4/25.
-//
-
-import Foundation
